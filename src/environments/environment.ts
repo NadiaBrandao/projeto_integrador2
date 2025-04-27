576453c5cd682a966c543999ce6f4639b977bd28
nadia brandao silva <nadiabrandao98@gmail.com>
2025-04-27 12:11:29 -0300

    Sua mensagem explicando o que mudou

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    api_url: 'http://localhost:3000'
+    api_url: 'https://projetointegrador2025-production-94db.up.railway.app' //'http://localhost:3000' teste
   }; 
